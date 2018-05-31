a688cab5b903c630ad5c26c6a10fbe6e9e613b19
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-31 15:56:53 +1000

    Passing data w/ event property binding: Load details of recipe based on the selection item from the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter,Output,Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  //Array of Recipe model object
+  
+  @Output('recipeItemSelected') recipeItemSelected=new EventEmitter<Recipe>();
   recipes:Recipe[]=[
     new Recipe('Lemon Tart', 'Creamy and dreamy lemon curd filling','https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/lemon-tart_A0.jpg?itok=gdqsna43&mtime=1379314018'),
     new Recipe('Easter Egg', 'The Egg in Verjuice','https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/03/28/16/heston-blumenthal-egg-verjuice.jpg?w968h681')
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipeEl:Recipe){
+    this.recipeItemSelected.emit(recipeEl);
+  }
 }
