57866af2bc58fcdfb487b01a2c9d0368ea737bff
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-05 12:36:12 +1000

    Improving the Reactive service with Observables(Subjects)

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,24 +1,21 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
-  ingredientsChanged= new EventEmitter<Ingredient[]>();
-  private ingredients:Ingredient[]=[];
+  ingredientsChanged = new Subject<Ingredient[]>();
+  private ingredients: Ingredient[] = [];
   constructor() {
-    }
-    getIngredients(){
-      return this.ingredients.slice();
-    }
-    addIngredient(ingredient:Ingredient){
-      this.ingredients.push(ingredient);
-      this.ingredientsChanged.emit(this.ingredients.slice());
-    }
-    addIngredients(ingredients:Ingredient[]){
-      // ingredients.forEach(element => {
-      //   this.ingredients.push(element);        
-      // });
-      //Add all ingredients to the array in one go!
-      this.ingredients.push(...ingredients)
-      this.ingredientsChanged.emit(this.ingredients.slice());
-    }
+  }
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+  addIngredients(ingredients: Ingredient[]) {
+    //Add all ingredients to the array in one go!
+    this.ingredients.push(...ingredients)
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
