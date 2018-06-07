ab07da3ca4bcdf7ca4f393c0d0e61155dd691957
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-07 11:09:03 +1000

    Angular template form for New ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef:ElementRef;
-  @ViewChild('amountInput') amountInputRef:ElementRef;
-  
-  constructor(private shoppingListService:ShoppingListService) { }
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem(){  
-     const ingName= this.nameInputRef.nativeElement.value;
-     const ingAmount= this.amountInputRef.nativeElement.value;
-     const newIngredient= new Ingredient(ingName,ingAmount);
-     this.shoppingListService.addIngredient(newIngredient);
+  onAddItem(newIngredientForm: NgForm) {
+    const formVal = newIngredientForm.value;
+    const newIngredient = new Ingredient(formVal.name, formVal.amount);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 }
