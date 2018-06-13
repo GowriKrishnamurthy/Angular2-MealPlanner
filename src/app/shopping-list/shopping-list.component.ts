744775f9ef6bd033aad4701497a3f76ce85c8aa2
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-13 12:00:16 +1000

    ngRx State - Selecting Ingredients from global store.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,6 +3,8 @@ import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'shopping-list',
@@ -10,19 +12,21 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit,OnDestroy {
-  ingredients:Ingredient[]=[];
+  shoppingListState:Observable<{ingredients:Ingredient[]}>;
   private subscription:Subscription;
-  constructor(private shoppingListService:ShoppingListService) { }
+  constructor(private shoppingListService:ShoppingListService,
+              private store:Store<{shoppingList:{ingredients:Ingredient[]}}>) { }
 
   ngOnInit() {
-    this.ingredients=this.shoppingListService.getIngredients();
+    // this.ingredients=this.shoppingListService.getIngredients();
+    this.shoppingListState=this.store.select('shoppingList');
 
-    this.subscription = this.shoppingListService.ingredientsChanged
-      .subscribe(
-         (ingredients:Ingredient[])=> {
-          this.ingredients=ingredients;
-         } 
-      );
+    // this.subscription = this.shoppingListService.ingredientsChanged
+    //   .subscribe(
+    //      (ingredients:Ingredient[])=> {
+    //       this.ingredients=ingredients;
+    //      } 
+    //   );
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
